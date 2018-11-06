2db6bda9b39922f95124c061fbd23139b53bcfb2
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2018-11-06 07:17:38 +0100

    Typo

--- src/main/scripts/win64/build_sonar.sh
+++ src/main/scripts/win64/build_sonar.sh
@@ -1,6 +1,6 @@
 cmake . \
 -DCMAKE_BUILD_TYPE=Release \
--DLIBRARY_OUTPUT_PATH="../../../../target/classes/linux64" \
+-DLIBRARY_OUTPUT_PATH="../../../../target/classes/win64" \
 -DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
 
 build-wrapper-linux-x86-64 --out-dir ../../../../target/sonar-cpp make
