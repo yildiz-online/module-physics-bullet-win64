0fb140a0fb7d20d6ace7d3d2d0b7c9396ad00310
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2018-11-06 06:59:00 +0100

    Add sonar

--- src/main/scripts/win64/build_sonar.sh
+++ src/main/scripts/win64/build_sonar.sh
@@ -1,9 +1,15 @@
-cmake . -DCMAKE_BUILD_TYPE=Release -DLIBRARY_OUTPUT_PATH="../../../../target/classes/linux64" -G "Unix Makefiles"
+cmake . \
+-DCMAKE_BUILD_TYPE=Release \
+-DLIBRARY_OUTPUT_PATH="../../../../target/classes/linux64" \
+-DCMAKE_TOOLCHAIN_FILE=mingw-toolchain.cmake
 
 build-wrapper-linux-x86-64 --out-dir ../../../../target/sonar-cpp make
+r1=$?
 
 rm -R CMakeFiles
 rm CMakeCache.txt
 rm cmake_install.cmake
 rm Makefile
+
+return $r1
 
